9d00ffab301fb5950ff4ef16777beefe43e0846e
Bill Mill <bill@billmill.org>
2015-06-07 16:59:36 -0400

    handle case where channel has no name

--- slackrtm/_client.py
+++ slackrtm/_client.py
@@ -42,7 +42,7 @@ class SlackClient(object):
         if "type" in data.keys():
             if data["type"] in ['channel_created', 'im_created']:
                 channel = data["channel"]
-                self.server.attach_channel(channel["name"], channel["id"], [])
+                self.server.attach_channel(channel.get("name", ""), channel["id"], [])
 
 
 class SlackNotConnected(Exception):
